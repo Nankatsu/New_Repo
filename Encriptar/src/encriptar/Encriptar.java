8f04c7364ba55ccfe362ad774d1c41d69138d305
Nankatsu <41645090+Nankatsu@users.noreply.github.com>
2018-07-24 11:33:15 -0600

    Add files via upload

---
+++ Encriptar/src/encriptar/Encriptar.java
@@ -0,0 +1,12 @@
+
+package encriptar;
+
+public class Encriptar {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new Prueba().setVisible(true);
+        });
+    }
+    
+}
